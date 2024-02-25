c2215d9b009c4d41dc46df1a9d9a69dde75e5d22
chetna dhir <chetnadhir042@gmail.com>
2024-02-25 21:12:36 +0530

    thisiscool

---
+++ index.js
@@ -0,0 +1,17 @@
+let isPaused = false;
+let speech = null;
+document.getElementById('textToSpeechButton').addEventListener('click', () => {
+    const text = document.body.innerText;
+    if (!speech) {
+        speech = new SpeechSynthesisUtterance(text);
+        speechSynthesis.speak(speech);
+    } else {
+        if (isPaused) {
+            speechSynthesis.resume();
+            isPaused = false;
+        } else {
+            speechSynthesis.pause();
+            isPaused = true;
+        }
+    }
+});
