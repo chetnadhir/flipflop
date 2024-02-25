dd6a4ddd1c80a907d577e46c0d541eb7edcf4ef6
cd <146971793+chetnadhir@users.noreply.github.com>
2024-02-25 21:25:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,28 @@
-const textToSpeechButton = document.getElementById('textToSpeechButton');
 let isPaused = false;
 let speech = null;
-let utteranceIndex = 0;
-let textChunks = [];
 
-function selectNextChunk() {
-    const range = document.createRange();
-    const textNode = textChunks[utteranceIndex];
-    if (textNode) {
-        range.selectNodeContents(textNode);
-        window.getSelection().removeAllRanges();
-        window.getSelection().addRange(range);
-    }
-}
-
-function startTextToSpeech() {
+document.getElementById('textToSpeechButton').addEventListener('click', () => {
     const text = document.body.innerText;
-    textChunks = text.split(' ').map(word => document.createTextNode(word + ' '));
-    utteranceIndex = 0;
-
     if (!speech) {
-        speech = new SpeechSynthesisUtterance();
-        speech.text = text;
-        speech.onend = () => {
-            if (utteranceIndex < textChunks.length - 1) {
-                utteranceIndex++;
-                selectNextChunk();
-                speechSynthesis.speak(speech);
-            } else {
-                isPaused = false;
-                speech = null;
-            }
-        };
-    }
-
-    if (!isPaused) {
+        speech = new SpeechSynthesisUtterance(text);
         speechSynthesis.speak(speech);
     } else {
-        speechSynthesis.resume();
-    }
-}
-
-function pauseTextToSpeech() {
-    speechSynthesis.pause();
-    isPaused = true;
-}
-
-textToSpeechButton.addEventListener('click', () => {
-    if (!speech || isPaused) {
-        startTextToSpeech();
-    } else {
-        pauseTextToSpeech();
+        if (isPaused) {
+            speechSynthesis.resume();
+            isPaused = false;
+        } else {
+            speechSynthesis.pause();
+            isPaused = true;
+        }
     }
 });
 
+// Mobile
+document.body.addEventListener('click', () => {
+    if (!speech) {
+        // Trigger speech synthesis on the first user interaction
+        const text = document.body.innerText;
+        speech = new SpeechSynthesisUtterance(text);
+        speechSynthesis.speak(speech);
+    }
+});
